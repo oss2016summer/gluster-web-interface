a2897824244992b8e18b76a80319a5f4856dc8f7
root <root@localhost.localdomain>
2016-08-19 18:03:00 +0900

    second

--- app/controllers/plainpage_controller.rb
+++ app/controllers/plainpage_controller.rb
@@ -1,6 +1,9 @@
 class PlainpageController < ApplicationController
 
   def index
+    unless user_signed_in?
+	redirect_to '/users/sign_in'
+    end
     flash[:success ] = "Success Flash Message: Welcome to GentellelaOnRails"
     #other alternatives are
     # flash[:warn ] = "Israel don't quite like warnings"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
