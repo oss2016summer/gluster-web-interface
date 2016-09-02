5a6c4d367cf6d991f6e58b3f183c5d6e351379b2
bee0005 <bee0005@naver.com>
2016-09-02 06:18:45 +0000

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     config = get_conf
-    @current_dir = config["project_path"]
+#    @current_dir = config["project_path"]
+    @current_dir = `~`
     file_directory(@current_dir)
   end
 end
