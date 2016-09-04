6716f426e9f5206c78f5e9826b781699da20f446
bee0005 <bee0005@naver.com>
2016-09-04 05:37:33 +0000

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,11 +10,7 @@ class HomeController < ApplicationController
   def make_directory
 	  current_dir = params[:current_dir]
 	  directory_name = params[:directory_name]
-	 #  `cd /#{current_dir}`
-	 # `cd /#{current_dir} & mkdir #{directory_name}`
-	  puts "mkdir #{current_dir}/#{directory_name}"
 	  `mkdir #{current_dir}/#{directory_name}`
-	  #output = `sshpass -p#{@config["host_password"]} ssh #{@config["host_port"]} #{@config["host_user"]}@#{@config["host_ip"]} gluster volume delete #{volume_name}`
-    redirect_to '/home/index'
+	  redirect_to '/home/index'
   end
 end
