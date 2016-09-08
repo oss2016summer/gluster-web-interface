1f6769466f858da6eec22d7816f616b223d1bde1
minkuk <alsrnr1210@gmail.com>
2016-09-08 08:51:24 +0000

    Update

--- script/setup.sh
+++ script/setup.sh
@@ -7,7 +7,12 @@ bundle install
 bin/rake db:migrate
 
 # make configuration file
+
 CONF_PATH=`pwd`/configure.conf
+
+if[ ls CONF_PATH | grep configure.conf ]; then
+echo "configure.conf exist"
+else
 touch $CONF_PATH
 echo "project_path=`pwd`" >> $CONF_PATH
 echo "server_name=gluster" >> $CONF_PATH
@@ -15,3 +20,4 @@ echo "host_ip=127.0.0.1" >> $CONF_PATH
 echo "host_port=" >> $CONF_PATH
 echo "host_user=root" >> $CONF_PATH
 echo "host_password=secret" >> $CONF_PATH
+fi
