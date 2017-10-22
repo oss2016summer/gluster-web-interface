08711ca80d72111225fa255b08c784aec58b33a3
Youngkyun <chancethecoder@gmail.com>
2017-10-22 13:54:03 +0900

    Add rbenv installation to setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -18,6 +18,13 @@ case "$(uname -s)" in
   Darwin)
     echo 'Mac OS X'
     #brew install sshpass
+    rbenv -v
+    if [ $? -ne 0 ]; then
+        echo 'Install rbenv'
+        brew install rbenv
+    fi
+    rbenv install 2.3.3 && rbenv rehash
+    rbenv local 2.3.3
     install
     setup
     ;;
@@ -32,6 +39,16 @@ case "$(uname -s)" in
     #  echo "os : ubuntu, use apt-get install"
     #  apt-get install sshpass
     #fi
+    rbenv -v
+    if [ $? -ne 0 ]; then
+        echo 'Install rbenv'
+        git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+        echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+        echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+        exec $SHELL
+    fi
+    rbenv install 2.3.3 && rbenv rehash
+    rbenv local 2.3.3
     install
     setup
     ;;
